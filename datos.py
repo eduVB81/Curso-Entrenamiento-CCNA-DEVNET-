a12f803c2c6dda6b52eb2fb14457dc068136f2d2
NetAcad DEVASC <changeme@example.com>
2026-04-15 05:30:19 +0000

    commit

---
+++ datos.py
@@ -0,0 +1,6 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = input("Ingrese su edad: ")
+sede = input("Ingrese su sede: ")
+
+print(f"\nUsuario: {nombre} {apellido}, {edad} años. Sede: {sede}")
